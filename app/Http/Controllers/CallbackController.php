9eb45e3b1a65589f2f368dabdf9167529ec24639
avnishtiwari456 <111576647+avnishtiwari456@users.noreply.github.com>
2022-12-12 15:02:48 +0530

    csd
    vw

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -25,50 +25,50 @@ class CallbackController extends Controller
         $data = file_get_contents('php://input');
         $decode_data = json_decode($data);
         dd($data);
-        switch ($decode_data->event) {
-            case 'RECHARGE_SUCCESS':
-                $param = $decode_data->param;
-                $operator = $param->operator;
-                $canumber = $decode_data->canumber;
-                $amount = $decode_data->amount;
-                $ackno = $decode_data->ackno;
-                $referenceid = $decode_data->referenceid;
-                $status = $decode_data->status;
-                $operatorid = $decode_data->operatorid;
-                $message = $decode_data->message;
-                echo json_encode(
-                    array(
-                        "status" => $status,
-                        "message" => $message
-                    )
-                );
-                $db->query('update set column status 1')->where();
-                break;
-            case 'RECHARGE_FAILURE':
-                # code...
-                break;
-            case 'MERCHANT_ONBOARDING':
-                echo json_encode(
-                    array(
-                        "status" => 200,
-                        "message" => "merchant onboarding success"
-                    )
-                );
-                break;
-            case 'CMS_BALANCE_INQUIRY':
-                echo json_encode(
-                    array(
-                        "status" => 200,
-                        "message" => "merchant onboarding success"
-                    )
-                );
-                break;
-            default:
-                echo json_encode(array(
-                    "status" => 404,
-                    "message" => "Not Found!"
-                ));
-                break;
-        }
+        // switch ($decode_data->event) {
+        //     case 'RECHARGE_SUCCESS':
+        //         $param = $decode_data->param;
+        //         $operator = $param->operator;
+        //         $canumber = $decode_data->canumber;
+        //         $amount = $decode_data->amount;
+        //         $ackno = $decode_data->ackno;
+        //         $referenceid = $decode_data->referenceid;
+        //         $status = $decode_data->status;
+        //         $operatorid = $decode_data->operatorid;
+        //         $message = $decode_data->message;
+        //         echo json_encode(
+        //             array(
+        //                 "status" => $status,
+        //                 "message" => $message
+        //             )
+        //         );
+        //         $db->query('update set column status 1')->where();
+        //         break;
+        //     case 'RECHARGE_FAILURE':
+        //         # code...
+        //         break;
+        //     case 'MERCHANT_ONBOARDING':
+        //         echo json_encode(
+        //             array(
+        //                 "status" => 200,
+        //                 "message" => "merchant onboarding success"
+        //             )
+        //         );
+        //         break;
+        //     case 'CMS_BALANCE_INQUIRY':
+        //         echo json_encode(
+        //             array(
+        //                 "status" => 200,
+        //                 "message" => "merchant onboarding success"
+        //             )
+        //         );
+        //         break;
+        //     default:
+        //         echo json_encode(array(
+        //             "status" => 404,
+        //             "message" => "Not Found!"
+        //         ));
+        //         break;
+        // }
     }
 }
