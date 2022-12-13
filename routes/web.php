dd3ecd06a1dd8b29b2156b9e151b3698281d3132
Ibrahim Nawab Qureshi <Ibrahimnawab303@gmail.com>
2022-12-14 01:53:08 +0500

    aasd

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::get('/test', function () {
              "operator"=>11,
              "canumber"=>8448998666,
              "amount"=>"10",
-             "referenceid"=>2633867893
+             "referenceid"=>2893867893
              );
      $res = json_decode(ApiController::post($service,$body));
      echo "<pre>";
