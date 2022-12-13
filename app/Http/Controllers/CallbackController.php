9f17534d0aac115e625c790020ff3bd1c050df30
avnishtiwari456 <111576647+avnishtiwari456@users.noreply.github.com>
2022-12-14 02:01:15 +0530

    ef
    ewf

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -28,9 +28,9 @@ class CallbackController extends Controller
     $testMailData = [
       'title' => 'Test Email From AllPHPTricks.com',
       'body' => 'This is the body of test email.',
-
+      dd('Success! Email has been sent successfully.');
   ];
-
+  dd($testMailData);
     Mail::to('jepecox303@bitvoo.com')->send(new CallBackMail($testMailData));
 
     dd('Success! Email has been sent successfully.');
