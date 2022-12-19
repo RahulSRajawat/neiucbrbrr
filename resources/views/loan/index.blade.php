e9ebf6bccbd82661a73063b521b7524f5f0f3300
Ibrahim Nawab Qureshi <Ibrahimnawab303@gmail.com>
2022-12-20 00:05:11 +0500

    loan responsive

--- resources/views/loan/index.blade.php
+++ resources/views/loan/index.blade.php
@@ -11,94 +11,96 @@
                                 <h4 class="card-title">Loan Application Form</h4>
                             </div>
                         </div>
-                        <table id="datatable" class="table table-striped table_style">
-                            <thead>
-                                <tr>
-                                    <th>S.No.</th>
-                                    <th>Loan Details</th>
-                                    <th>Apply</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Personal Loan</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Business Loan</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Gold Loan</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Home Loan (Salaried Employed)</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>5</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Home Loan (Self Employed)</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>6</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Loan Against Property (Salaried Employed)</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>7</td>
-                                    <td>
-                                        <div class="align-items-center">
-                                            <h6>Loan Against Property (Self Employed)</h6>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="table-responsive">
+                            <table id="datatable" class="table table-striped table_style">
+                                <thead>
+                                    <tr>
+                                        <th>S.No.</th>
+                                        <th>Loan Details</th>
+                                        <th>Apply</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Personal Loan</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Business Loan</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Gold Loan</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Home Loan (Salaried Employed)</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Home Loan (Self Employed)</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Loan Against Property (Salaried Employed)</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>7</td>
+                                        <td>
+                                            <div class="align-items-center">
+                                                <h6>Loan Against Property (Self Employed)</h6>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div><button type="button" class="btn btn-soft-primary">Apply</button></div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <div class="col-sm-12 col-lg-6">
@@ -106,8 +108,9 @@
                         <div class="card-header d-flex align-items-center justify-content-between pb-4">
                             <div class="card-body p-0">
                                 <div class="user-post">
-                                    <a href="javascript:void(0);"><img src="{{ asset('assets/images/pages/001-page.webp') }}"
-                                            alt="post-image" class="img-fluid"></a>
+                                    <a href="javascript:void(0);"><img
+                                            src="{{ asset('assets/images/pages/001-page.webp') }}" alt="post-image"
+                                            class="img-fluid"></a>
                                 </div>
                             </div>
                         </div>
