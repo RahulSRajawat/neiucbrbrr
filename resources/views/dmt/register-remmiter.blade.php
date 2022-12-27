2c862e4bb81f1d1f7ecbada436b4f9bf0e44944d
Ibrahim Nawab Qureshi <Ibrahimnawab303@gmail.com>
2022-12-28 00:07:07 +0500

    Add Register Remmiter Form Fields

--- resources/views/dmt/register-remmiter.blade.php
+++ resources/views/dmt/register-remmiter.blade.php
@@ -18,14 +18,13 @@
                                     <div class="alert alert-warning">
                                         {{ session('status') }}
                                     </div>
-                                @endif 
+                                @endif
                                 <div class="row">
                                     <div class="col-md-12 mb-3">
                                         <label for="">Customer Mobile Number</label>
                                         <input type="number" readonly min="0" class="form-control"
-                                            placeholder="Enter 10 digit customer mobile number" name="phone"
-                                            value="{{ $phone }}" pattern="[0-9]{10,10}"
-                                            title="10 Digit Mobile Number" maxlength="10">
+                                            placeholder="Enter 10 digit customer mobile number" value="{{ $phone }}"
+                                            pattern="[0-9]{10,10}" title="10 Digit Mobile Number" maxlength="10">
                                     </div>
                                     <div class="col-md-12 mb-3">
                                         <button type="button" class="btn btn-primary" disabled>Submit</button>
@@ -52,12 +51,34 @@
                                     </div>
                                 @endif
                                 <div class="row">
+                                    <input type="hidden" name="stateresp" value="{{ $stateresp }}">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="">First Name</label>
+                                        <input type="text" class="form-control" name="fname" placeholder="First Name">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="">Last Name</label>
+                                        <input type="text" class="form-control" name="lname" placeholder="First Name">
+                                    </div>
                                     <div class="col-md-12 mb-3">
-                                        <label for="">Customer Mobile Number</label>
-                                        <input type="number" readonly min="0" class="form-control"
-                                            placeholder="Enter 10 digit customer mobile number" name="phone"
-                                            value="{{ $phone }}" pattern="[0-9]{10,10}"
-                                            title="10 Digit Mobile Number" maxlength="10">
+                                        <label for="">Mobile Number</label>
+                                        <input type="number" readonly min="0" class="form-control" placeholder="Enter 10 digit mobile number" name="phone" value="{{ $phone }}" pattern="[0-9]{10,10}" title="10 Digit Mobile Number" maxlength="10">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="">OTP</label>
+                                        <input type="number" min="0" class="form-control" name="otp" placeholder="6 Digit Number OTP">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="">Pin Code</label>
+                                        <input type="number" min="0" class="form-control" name="pin_code" placeholder="6 Digit Number Pin Code">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="">Date Of Birth</label>
+                                        <input type="date" class="form-control" name="dob" placeholder="Date Of Birth">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="">Address</label>
+                                        <input type="text" class="form-control" name="address" placeholder="Address">
                                     </div>
                                     <div class="col-md-12 mb-3">
                                         <button type="submit" class="btn btn-primary">Submit</button>
