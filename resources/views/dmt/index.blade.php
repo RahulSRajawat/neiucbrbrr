d3c0ed48d7aff6f5548797ecbd7b7d4692eadcad
Ibrahim Nawab Qureshi <Ibrahimnawab303@gmail.com>
2022-12-27 05:01:56 +0500

    asdas

--- resources/views/dmt/index.blade.php
+++ resources/views/dmt/index.blade.php
@@ -14,11 +14,14 @@
                                     <li>Month Limit :</li>
                                 </ul>
                                 @foreach ($details as $detail)
-                                <ul class="details">
+                                @php
+                                    print_r($detail);
+                                @endphp
+                                {{-- <ul class="details">
                                     <li><i class="fa fa-user"></i> {{ $detail->fname }} {{ $detail->lname }}</li>
                                     <li><i class="fa fa-mobile"></i> {{ $detail->mobile }}</li>
                                     <li class="rem-limit"><i class="fa fa-inr"></i> 25000.00</li>
-                                </ul>
+                                </ul> --}}
                                 @endforeach
                             </div>
                             <div class="benelist_btn">
