8ffdf8dea21e1108ce7a066df4a194ae966b17ea
Ibrahim Nawab Qureshi <Ibrahimnawab303@gmail.com>
2022-12-28 01:52:58 +0500

    add beneficiary

---
+++ database/migrations/2022_12_27_204413_create_dmt_beneficiary.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDmtBeneficiary extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dmt_beneficiary', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("bene_id");
+            $table->string("mobile");
+            $table->string("benename");
+            $table->bigInteger("bankid");
+            $table->bigInteger("accno");
+            $table->bigInteger("pincode");
+            $table->date("dob");
+            $table->string("address");
+            $table->string("ifsc")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dmt_beneficiary');
+    }
+}
